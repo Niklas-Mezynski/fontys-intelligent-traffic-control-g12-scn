5f2dc5aaf77a53188749305b17e77c6863013dbe
danielsevov <70882890+danielsevov@users.noreply.github.com>
2021-11-03 12:00:22 +0100

    Make new test public

--- implementation/BusinessLogic/src/test/java/trafficLights/SimplePedestrianTrafficLightTest.java
+++ implementation/BusinessLogic/src/test/java/trafficLights/SimplePedestrianTrafficLightTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-class SimplePedestrianTrafficLightTest {
+public class SimplePedestrianTrafficLightTest {
 
     SimplePedestrianTrafficLight trafficLight = new SimplePedestrianTrafficLight(GermanyPedestrianLightBehaviour.RED_LIGHT);
 
