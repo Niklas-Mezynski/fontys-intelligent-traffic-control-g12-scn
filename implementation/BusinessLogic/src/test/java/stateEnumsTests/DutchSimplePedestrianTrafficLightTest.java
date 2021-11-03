fecaaa15db8452a02b2897674ff351715fd1a762
Henrik1704 <70882779+Henrik1704@users.noreply.github.com>
2021-11-03 12:26:08 +0100

    Test for DutchSimplePedestrianTrafficLight

---
+++ implementation/BusinessLogic/src/test/java/stateEnumsTests/DutchSimplePedestrianTrafficLightTest.java
@@ -0,0 +1,38 @@
+package stateEnumsTests;
+
+import lightBehaviour.DutchPedestrianLightBehaviour;
+import org.junit.jupiter.api.Test;
+import trafficLights.SimplePedestrianTrafficLight;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class DutchSimplePedestrianTrafficLightTest {
+
+    SimplePedestrianTrafficLight dutchLight = new SimplePedestrianTrafficLight(DutchPedestrianLightBehaviour.RED_LIGHT);
+
+
+    @Test
+    public void getCurrentStateShouldReturnCorrectStateTest(){
+        assertThat(dutchLight.getCurrentState()).isEqualTo(DutchPedestrianLightBehaviour.RED_LIGHT);
+    }
+
+    @Test
+    public void changeStateMethodTest(){
+       dutchLight.changeState(DutchPedestrianLightBehaviour.GREEN_BLINKING_LIGHT);
+       assertThat(dutchLight.getCurrentState()).isEqualTo(DutchPedestrianLightBehaviour.GREEN_BLINKING_LIGHT);
+
+    }
+    @Test
+    public void startTrafficTest(){
+        dutchLight.startTraffic();
+        assertThat(dutchLight.getCurrentState()).isEqualTo(DutchPedestrianLightBehaviour.GREEN_LIGHT);
+
+    }
+
+    @Test
+    public void stopTrafficTest(){
+        dutchLight.stopTraffic();
+        assertThat(dutchLight.getCurrentState()).isEqualTo(DutchPedestrianLightBehaviour.RED_LIGHT);
+
+    }
+    
+}
