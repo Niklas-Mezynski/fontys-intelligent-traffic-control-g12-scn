6febaa2032dedc0c7d4979773f8d0470dc040761
Henrik1704 <70882779+Henrik1704@users.noreply.github.com>
2021-11-04 16:11:29 +0100

    adding test for the behaviour

---
+++ implementation/BusinessLogic/src/test/java/lightBehaviours/DutchSimplePedestrianTrafficLightTest2.java
@@ -0,0 +1,59 @@
+package lightBehaviours;
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class DutchSimplePedestrianTrafficLightTest2 {
+    DutchPedestrianLightBehaviour redLight = DutchPedestrianLightBehaviour.RED_LIGHT;
+    DutchPedestrianLightBehaviour greenLight = DutchPedestrianLightBehaviour.GREEN_LIGHT;
+    DutchPedestrianLightBehaviour greenBlinkingLight = DutchPedestrianLightBehaviour.GREEN_BLINKING_LIGHT;
+
+    @Test
+    public void redLightGetNextTest(){
+        assertThat(redLight.getNext()).isEqualTo(greenLight);
+    }
+
+    @Test
+    void redLightCheckValues() {
+
+        SoftAssertions.assertSoftly(s -> {
+            s.assertThat(redLight.canPass()).isFalse();
+            s.assertThat(redLight.getName()).isEqualTo("Red Light");
+            s.assertThat(redLight.length()).isEqualTo(0);
+        });
+
+    }
+
+    @Test
+    void greenLightGetNextTest() {
+        assertThat(greenLight.getNext()).isEqualTo(greenBlinkingLight);
+    }
+
+    @Test
+    void greenBlinkingLightGetNextTest() {
+        assertThat(greenBlinkingLight.getNext()).isEqualTo(redLight);
+    }
+
+    @Test
+    void greenLightCheckValues() {
+
+        SoftAssertions.assertSoftly(s -> {
+            s.assertThat(greenLight.canPass()).isTrue();
+            s.assertThat(greenLight.getName()).isEqualTo("Green Light");
+            s.assertThat(greenLight.length()).isEqualTo(2);
+        });
+
+    }
+
+    @Test
+    void greenBlinkingLightCheckValues() {
+
+        SoftAssertions.assertSoftly(s -> {
+            s.assertThat(greenBlinkingLight.canPass()).isTrue();
+            s.assertThat(greenBlinkingLight.getName()).isEqualTo("Green Blinking Light");
+            s.assertThat(greenBlinkingLight.length()).isEqualTo(2);
+        });
+
+    }
+}
