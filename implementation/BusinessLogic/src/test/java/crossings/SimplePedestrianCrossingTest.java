5612fbcd24935b6df3767814fe2bde38308a6859
Niklas-Mezynski <70883487+Niklas-Mezynski@users.noreply.github.com>
2021-11-18 12:25:35 +0100

    Just a name change

--- implementation/BusinessLogic/src/test/java/crossings/SimplePedestrianCrossingTest.java
+++ implementation/BusinessLogic/src/test/java/crossings/SimplePedestrianCrossingTest.java
@@ -5,22 +5,24 @@ import org.mockito.InOrder;
 import org.mockito.Mockito;
 import trafficLights.SimplePedestrianTrafficLight;
 
+import static org.mockito.Mockito.*;
+
 public class SimplePedestrianCrossingTest {
 
     @Test
-    void t01() throws InterruptedException {
-        SimplePedestrianTrafficLight horizontal = Mockito.mock(SimplePedestrianTrafficLight.class);
-        SimplePedestrianTrafficLight vertical = Mockito.mock(SimplePedestrianTrafficLight.class);
+    void t01testCrossingSequence() throws InterruptedException {
+        SimplePedestrianTrafficLight horizontal = mock(SimplePedestrianTrafficLight.class);
+        SimplePedestrianTrafficLight vertical = mock(SimplePedestrianTrafficLight.class);
         SimpleCrossing simpleCrossing = new SimpleCrossing();
         simpleCrossing.addPedestrianTrafficLightPair(horizontal, vertical);
 
-        InOrder inOrder = Mockito.inOrder(horizontal, vertical);
+        InOrder inOrder = inOrder(horizontal, vertical);
 
         simpleCrossing.activate(1);
-        inOrder.verify(vertical, Mockito.times(1)).stopTraffic();
-        inOrder.verify(horizontal, Mockito.times(1)).startTraffic();
-        inOrder.verify(horizontal, Mockito.times(1)).stopTraffic();
-        inOrder.verify(vertical, Mockito.times(1)).startTraffic();
+        inOrder.verify(vertical, times(1)).stopTraffic();
+        inOrder.verify(horizontal, times(1)).startTraffic();
+        inOrder.verify(horizontal, times(1)).stopTraffic();
+        inOrder.verify(vertical, times(1)).startTraffic();
         simpleCrossing.deactivate();
     }
 
