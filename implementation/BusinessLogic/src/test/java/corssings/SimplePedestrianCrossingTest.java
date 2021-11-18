3e637a930bfbc5e77934f38f5abccfa4b12dfb18
Niklas-Mezynski <70883487+Niklas-Mezynski@users.noreply.github.com>
2021-11-18 12:08:15 +0100

    PedestrianCrossingTest

---
+++ implementation/BusinessLogic/src/test/java/corssings/SimplePedestrianCrossingTest.java
@@ -0,0 +1,31 @@
+package corssings;
+
+import crossings.SimpleCrossing;
+import factories.SimpleTrafficLightFactory;
+import interfaces.PedestrianTrafficLight;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.verification.VerificationMode;
+import trafficLights.SimplePedestrianTrafficLight;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SimplePedestrianCrossingTest {
+
+    @Test
+    void t01() throws InterruptedException {
+        SimplePedestrianTrafficLight horizontal = Mockito.mock(SimplePedestrianTrafficLight.class);
+        SimplePedestrianTrafficLight vertical = Mockito.mock(SimplePedestrianTrafficLight.class);
+        SimpleCrossing simpleCrossing = new SimpleCrossing();
+        simpleCrossing.addPedestrianTrafficLightPair(horizontal, vertical);
+
+        simpleCrossing.activate();
+        Mockito.verify(vertical, Mockito.times(1)).stopTraffic();
+        Mockito.verify(horizontal, Mockito.times(1)).startTraffic();
+        Thread.sleep(5005);
+        simpleCrossing.deactivate();
+        Mockito.verify(horizontal, Mockito.times(1)).stopTraffic();
+        Mockito.verify(vertical, Mockito.times(1)).startTraffic();
+    }
+
+}
