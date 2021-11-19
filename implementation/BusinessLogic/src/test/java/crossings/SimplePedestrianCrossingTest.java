8fb74911e4ded254aef18241096b041adcdd934a
Niklas-Mezynski <70883487+Niklas-Mezynski@users.noreply.github.com>
2021-11-19 11:27:07 +0100

    Test for combined crossing (street and pedestrian)

--- implementation/BusinessLogic/src/test/java/crossings/SimplePedestrianCrossingTest.java
+++ implementation/BusinessLogic/src/test/java/crossings/SimplePedestrianCrossingTest.java
@@ -3,6 +3,7 @@ package crossings;
 import org.junit.jupiter.api.Test;
 import org.mockito.InOrder;
 import trafficLights.SimplePedestrianTrafficLight;
+import trafficLights.SimpleStreetTrafficLight;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -30,4 +31,29 @@ public class SimplePedestrianCrossingTest {
         inOrder.verify(vertical, times(1)).startTraffic();
     }
 
+    @Test
+    void t02testCrossingSequence() throws InterruptedException {
+        SimplePedestrianTrafficLight horizontalPedestrian = mock(SimplePedestrianTrafficLight.class);
+        SimpleStreetTrafficLight horizontalStreet = mock(SimpleStreetTrafficLight.class);
+        SimplePedestrianTrafficLight verticalPedestrian = mock(SimplePedestrianTrafficLight.class);
+        SimpleStreetTrafficLight verticalStreet = mock(SimpleStreetTrafficLight.class);
+        SimpleCrossing simpleCrossing = new SimpleCrossing();
+        simpleCrossing.addPedestrianTrafficLightPair(horizontalPedestrian, verticalPedestrian);
+        simpleCrossing.addStreetTrafficLightPair(horizontalStreet, verticalStreet);
+
+        InOrder inOrder = inOrder(horizontalPedestrian, verticalPedestrian, horizontalStreet, verticalStreet);
+        CountDownLatch lock = new CountDownLatch(1);
+        int millisSignalLength = 1;
+        simpleCrossing.activate(millisSignalLength);
+        lock.await(millisSignalLength * 50, TimeUnit.MILLISECONDS);
+        simpleCrossing.deactivate();
+        inOrder.verify(verticalPedestrian, times(1)).stopTraffic();
+        inOrder.verify(verticalStreet, times(1)).stopTraffic();
+        inOrder.verify(horizontalPedestrian, times(1)).startTraffic();
+        inOrder.verify(horizontalStreet, times(1)).startTraffic();
+        inOrder.verify(horizontalPedestrian, times(1)).stopTraffic();
+        inOrder.verify(horizontalStreet, times(1)).stopTraffic();
+        inOrder.verify(verticalPedestrian, times(1)).startTraffic();
+        inOrder.verify(verticalStreet, times(1)).startTraffic();
+    }
 }
